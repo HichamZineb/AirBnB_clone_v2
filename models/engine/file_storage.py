789b47e88291acbabb26792fb3ced9067f380502
HichamZineb <hichamelamine@hotmail.com>
2023-09-20 06:56:56 -0700

    update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines the FileStorage class."""
+"""This module defines a class to manage file storage for hbnb clone"""
 import json
 from models.base_model import BaseModel
 from models.amenity import Amenity
@@ -11,36 +11,35 @@ from models.user import User
 
 
 class FileStorage:
-    """Represents a storage engine."""
+    """This class manages storage of hbnb models in JSON format"""
 
     __file_path = "file.json"
     __objects = {}
 
     def all(self, cls=None):
-        """Return a dictionary of instantiated objects in __objects."""
+        """Returns a dictionary of models currently in storage"""
         if cls is not None:
-            if isinstance(cls, str):
+            if type(cls) == str:
                 cls = eval(cls)
             cls_dict = {}
-            for k, v in self.__objects.items():
-                if isinstance(v, cls):
-                    cls_dict[k] = v
+            for key, value in self.__objects.items():
+                if type(value) == cls:
+                    cls_dict[key] = value
             return cls_dict
         return self.__objects
 
     def new(self, obj):
-        """Set in __objects obj with key <obj_class_name>.id."""
-        key = "{}.{}".format(type(obj).__name__, obj.id)
-        self.__objects[key] = obj
+        """Adds new object to storage dictionary"""
+        self.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
 
     def save(self):
-        """Serialize __objects to the JSON file __file_path."""
+        """Saves storage dictionary to file"""
         odict = {o: self.__objects[o].to_dict() for o in self.__objects.keys()}
         with open(self.__file_path, "w", encoding="utf-8") as f:
             json.dump(odict, f)
 
     def reload(self):
-        """Deserialize the JSON file __file_path to __objects, if it exists."""
+        """Loads storage dictionary from file"""
         try:
             with open(self.__file_path, "r", encoding="utf-8") as f:
                 for o in json.load(f).values():
@@ -51,13 +50,12 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
-        """Delete a given object from __objects, if it exists."""
+        """Deletes object from file"""
         try:
-            key = "{}.{}".format(type(obj).__name__, obj.id)
-            del self.__objects[key]
+            del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
         except (AttributeError, KeyError):
             pass
 
     def close(self):
-        """Call the reload method."""
+        """Closes the file"""
         self.reload()
