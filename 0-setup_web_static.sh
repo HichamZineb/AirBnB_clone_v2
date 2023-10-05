6d888dc9d4961efb96507a67283f97ef53c3785e
HichamZineb <hichamelamine@hotmail.com>
2023-10-05 03:04:53 -0700

    newfile 0-setup_web_static.sh

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Install Nginx if it not already installed.
+
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
+
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu /data
+chgrp -R ubuntu /data
+
+sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+
+sudo service nginx restart
+
+exit 0
