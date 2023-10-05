43c43a8326d7237a1eaba29c4b658067de711c78
HichamZineb <hichamelamine@hotmail.com>
2023-10-05 03:50:03 -0700

    update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,8 +10,8 @@ sudo mkdir -p /data/web_static/shared/
 echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu /data
-chgrp -R ubuntu /data
+sudo chown -R ubuntu /data
+sudo chgrp -R ubuntu /data
 
 sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 
