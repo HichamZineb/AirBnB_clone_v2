b0a23d63d470d4775b9e5dec3d37b1550eec3717
HichamZineb <hichamelamine@hotmail.com>
2023-10-22 14:30:07 -0700

    update base_model.py place.py

--- models/place.py
+++ models/place.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
-from models.base_model import BaseModel
-from models.base_model import Base
+"""Defines the Place class."""
 import models
 from os import getenv
+from models.base_model import Base
+from models.base_model import BaseModel
 from models.amenity import Amenity
 from models.review import Review
 from sqlalchemy import Column
@@ -12,21 +12,39 @@ from sqlalchemy import ForeignKey
 from sqlalchemy import Integer
 from sqlalchemy import String
 from sqlalchemy import Table
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 
-place_table = Table(
-    "place_amenity",
-    Base.metadata,
-    Column("place_id", String(60), ForeignKey("places.id"),
-           primary_key=True, nullable=False),
-    Column("amenity_id", String(60), ForeignKey("amenities.id"),
-           primary_key=True, nullable=False)
-)
+
+association_table = Table("place_amenity", Base.metadata,
+                          Column("place_id", String(60),
+                                 ForeignKey("places.id"),
+                                 primary_key=True, nullable=False),
+                          Column("amenity_id", String(60),
+                                 ForeignKey("amenities.id"),
+                                 primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
-    """ A place to stay """
+    """Represents a Place for a MySQL database.
+
+    Inherits from SQLAlchemy Base and links to the MySQL table places.
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store places.
+        city_id (sqlalchemy String): The place's city id.
+        user_id (sqlalchemy String): The place's user id.
+        name (sqlalchemy String): The name.
+        description (sqlalchemy String): The description.
+        number_rooms (sqlalchemy Integer): The number of rooms.
+        number_bathrooms (sqlalchemy Integer): The number of bathrooms.
+        max_guest (sqlalchemy Integer): The maximum number of guests.
+        price_by_night (sqlalchemy Integer): The price by night.
+        latitude (sqlalchemy Float): The place's latitude.
+        longitude (sqlalchemy Float): The place's longitude.
+        reviews (sqlalchemy relationship): The Place-Review relationship.
+        amenities (sqlalchemy relationship): The Place-Amenity relationship.
+        amenity_ids (list): An id list of all linked amenities.
+    """
     __tablename__ = "places"
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
@@ -40,29 +58,29 @@ class Place(BaseModel, Base):
     longitude = Column(Float)
     reviews = relationship("Review", backref="place", cascade="delete")
     amenities = relationship("Amenity", secondary="place_amenity",
-            viewonly=False, back_populates='place_amenities')
+                             viewonly=False)
     amenity_ids = []
 
     if getenv("HBNB_TYPE_STORAGE", None) != "db":
         @property
         def reviews(self):
-            """ Get related Reviews """
-            related_reviews = []
+            """Get a list of all linked Reviews."""
+            review_list = []
             for review in list(models.storage.all(Review).values()):
                 if review.place_id == self.id:
-                    related_reviews.append(review)
-            return related_reviews
+                    review_list.append(review)
+            return review_list
 
         @property
-        def amenities(self):
-            """ Get related Amenities """
-            related_amenities = []
+        def get_amenities(self):
+            """Get/set linked Amenities."""
+            amenity_list = []
             for amenity in list(models.storage.all(Amenity).values()):
                 if amenity.id in self.amenity_ids:
-                    related_amenities.append(amenity)
-            return related_amenities
+                    amenity_list.append(amenity)
+            return amenity_list
 
-        @amenities.setter
-        def amenities(self, value):
+        @get_amenities.setter
+        def get_amenities(self, value):
             if type(value) == Amenity:
                 self.amenity_ids.append(value.id)
